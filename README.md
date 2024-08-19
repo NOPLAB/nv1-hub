ad98df829c1da985d7c6d2c4c42c3f9ec136b616
nop <noplab90@gmail.com>
2024-08-20 01:15:52 +0900

    ui-tester impl

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ cargo run
 ```
 
 ```bash
+https://github.com/Rust-SDL2/rust-sdl2#windows-msvc
 cd nv1-hub-ui-tester
 cargo run
 ```
--- nv1-hub-ui-tester/src/main.rs
+++ nv1-hub-ui-tester/src/main.rs
@@ -7,4 +7,60 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 }
 
 #[cfg(not(feature = "no_std"))]
-fn main() {}
+fn main() -> anyhow::Result<()> {
+    use embedded_graphics::{
+        mono_font::{ascii::FONT_6X9, MonoTextStyle},
+        pixelcolor::BinaryColor,
+        prelude::*,
+        primitives::{Circle, Line, PrimitiveStyle, Rectangle},
+        text::Text,
+    };
+    use embedded_graphics_simulator::{
+        BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
+    };
+
+    println!("Hello, world!");
+
+    let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(128, 64));
+    let output_settings = OutputSettingsBuilder::new()
+        .theme(BinaryColorTheme::OledWhite)
+        .build();
+    let mut window = Window::new("SSD1306", &output_settings);
+
+    let line_style = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
+    let text_style = MonoTextStyle::new(&FONT_6X9, BinaryColor::On);
+
+    Circle::new(Point::new(72, 8), 48)
+        .into_styled(line_style)
+        .draw(&mut display)?;
+
+    Line::new(Point::new(48, 16), Point::new(8, 16))
+        .into_styled(line_style)
+        .draw(&mut display)?;
+
+    Line::new(Point::new(48, 16), Point::new(64, 32))
+        .into_styled(line_style)
+        .draw(&mut display)?;
+
+    Rectangle::new(Point::new(79, 15), Size::new(34, 34))
+        .into_styled(line_style)
+        .draw(&mut display)?;
+
+    Text::new("Hello World!", Point::new(5, 5), text_style).draw(&mut display)?;
+
+    'running: loop {
+        window.update(&display);
+
+        for event in window.events() {
+            match event {
+                SimulatorEvent::Quit => break 'running,
+                SimulatorEvent::KeyDown { keycode, .. } => {
+                    println!("Key pressed: {:?}", keycode);
+                }
+                _ => {}
+            }
+        }
+    }
+
+    Ok(())
+}
