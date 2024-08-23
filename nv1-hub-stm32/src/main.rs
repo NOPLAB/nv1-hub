662bac58b72753ba3fc810868e08a6a3d0f25f87
nop <noplab90@gmail.com>
2024-08-23 17:23:02 +0900

    impl

--- nv1-hub-stm32/src/main.rs
+++ nv1-hub-stm32/src/main.rs
@@ -6,12 +6,17 @@ mod omni;
 
 extern crate alloc;
 
-use core::{borrow::Borrow, cell::RefCell};
+use core::{
+    borrow::{Borrow, BorrowMut},
+    cell::RefCell,
+    ops::Deref,
+};
 
 use alloc::vec::Vec;
 use defmt::error;
+use embassy_futures::select::{select, select3};
 use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, mutex::Mutex};
-use embassy_time::{Duration, Timer};
+use embassy_time::{with_timeout, Duration, Instant, Timer};
 use embedded_alloc::Heap;
 
 #[global_allocator]
@@ -30,10 +35,11 @@ use embassy_stm32::{
 };
 use embassy_time::Delay;
 use embedded_graphics::prelude::Size;
-use libm::{asinf, atan2f, cosf, powf, sinf, sqrtf};
+use libm::{cosf, powf, sinf, sqrtf};
 use num_traits::{AsPrimitive, Num};
 use nv1_hub_ui::{HubUi, HubUiEvent, HubUiOptions};
 
+use nv1_msg::hub::HubMsgPackTx;
 #[cfg(not(feature = "defmt"))]
 use panic_halt as _;
 use ssd1306::{mode::DisplayConfig, size::DisplaySize128x64, I2CDisplayInterface, Ssd1306};
@@ -71,28 +77,43 @@ union MdData {
 
 static BB: BBBuffer<{ bno08x_rvc::BUFFER_SIZE }> = BBBuffer::new();
 
-static G_MSG_PACK_RX: Mutex<ThreadModeRawMutex, RefCell<nv1_msg::HubMsgPackRx>> =
-    Mutex::new(RefCell::new(nv1_msg::HubMsgPackRx {
-        vel: nv1_msg::Velocity {
-            linear_x: 0.0,
-            linear_y: 0.0,
-            angular_z: 0.0,
+static G_UART: Mutex<
+    ThreadModeRawMutex,
+    Option<Uart<'static, peripherals::USART3, peripherals::DMA1_CH3, peripherals::DMA1_CH1>>,
+> = Mutex::new(None);
+
+static G_MSG_RX: Mutex<ThreadModeRawMutex, nv1_msg::hub::HubMsgPackRx> =
+    Mutex::new(nv1_msg::hub::HubMsgPackRx {
+        vel: nv1_msg::hub::Velocity {
+            x: 0.0,
+            y: 0.0,
+            angle: 0.0,
         },
         kick: false,
+    });
+
+static G_MSG_TX: Mutex<ThreadModeRawMutex, RefCell<nv1_msg::hub::HubMsgPackTx>> =
+    Mutex::new(RefCell::new(nv1_msg::hub::HubMsgPackTx {
+        pause: false,
+        shutdown: false,
+        reboot: false,
+        vel: nv1_msg::hub::Velocity {
+            x: 0.0,
+            y: 0.0,
+            angle: 0.0,
+        },
+        ir: nv1_msg::hub::Ir {
+            x: 0.0,
+            y: 0.0,
+            strength: 0.0,
+        },
+        line: nv1_msg::hub::Line {
+            x: 0.0,
+            y: 0.0,
+            strength: 0.0,
+        },
+        have_ball: false,
     }));
-
-fn quaternion_to_euler_angle(q: &sh2::sh2_RotationVector_t) -> (f32, f32, f32) {
-    let q0 = q.real;
-    let q1 = q.i;
-    let q2 = q.j;
-    let q3 = q.k;
-
-    let roll = atan2f(2.0 * (q0 * q1 + q2 * q3), 1.0 - 2.0 * (q1 * q1 + q2 * q2));
-    let pitch = asinf(2.0 * (q0 * q2 - q3 * q1));
-    let yaw = atan2f(2.0 * (q0 * q3 + q1 * q2), 1.0 - 2.0 * (q2 * q2 + q3 * q3));
-
-    (roll, pitch, yaw)
-}
 
 fn generate_adc_vec<T>(sin: &mut [T], cos: &mut [T], offset: f32, one_angle: f32, mul: f32)
 where
@@ -149,6 +170,8 @@ async fn main(spawner: Spawner) {
     )
     .unwrap();
 
+    G_UART.lock().await.replace(uart_jetson);
+
     let mut uart4_config = Config::default();
     uart4_config.baudrate = 115200;
     let mut uart_md = Uart::new(
@@ -175,6 +198,12 @@ async fn main(spawner: Spawner) {
     )
     .unwrap();
 
+    let mut gpio_reset = Output::new(p.PA0, Level::High, embassy_stm32::gpio::Speed::Low);
+    gpio_reset.set_low();
+    Timer::after(Duration::from_millis(10)).await;
+    gpio_reset.set_high();
+    Timer::after(Duration::from_millis(100)).await;
+
     let mut delay = Delay;
     let mut adc1 = Adc::new(p.ADC1, &mut delay);
     adc1.set_sample_time(embassy_stm32::adc::SampleTime::Cycles3);
@@ -200,8 +229,6 @@ async fn main(spawner: Spawner) {
         1.0,
     );
 
-    info!("ADC Line Sin: {:?}", adc_line_sin);
-
     let mut adc_ir_sin = [0.0f32; 16];
     let mut adc_ir_cos = [0.0f32; 16];
 
@@ -214,6 +241,9 @@ async fn main(spawner: Spawner) {
     );
 
     let mut gpio_ui_toggle = Input::new(p.PC12, Pull::None);
+    let mut gpio_ui_up = Input::new(p.PC13, Pull::None);
+    let mut gpio_ui_down = Input::new(p.PC14, Pull::None);
+    let mut gpio_ui_enter = Input::new(p.PC15, Pull::None);
 
     let mut config = i2c::Config::default();
     config.timeout = Duration::from_millis(100);
@@ -245,11 +275,6 @@ async fn main(spawner: Spawner) {
     let _ = ui.update(&HubUiEvent::None);
 
     ssd1306.flush().unwrap();
-    let mut gpio_reset = Output::new(p.PA0, Level::High, embassy_stm32::gpio::Speed::Low);
-    gpio_reset.set_low();
-    Timer::after(Duration::from_millis(10)).await;
-    gpio_reset.set_high();
-    Timer::after(Duration::from_millis(100)).await;
 
     let (mut proc, mut parser) = match bno08x_rvc::create(BB.borrow()) {
         Ok((proc, pars)) => (proc, pars),
@@ -263,8 +288,15 @@ async fn main(spawner: Spawner) {
     let mut _pitch = 0.0;
     let mut yaw = 0.0;
 
-    let mut angle_pid = pid::Pid::new(0.0, 100.0); // TODO
+    let mut prev_yaw = 0.0;
+
+    let mut prev_system_time_ms = 0;
+
+    let mut angle_pid: pid::Pid<f32> = pid::Pid::new(0.0, 100.0);
     angle_pid.p(1.2, 100.0);
+
+    let mut speed_pid: pid::Pid<f32> = pid::Pid::new(0.0, 100.0);
+    speed_pid.p(0.08, 100.0);
 
     const WHEEL_R: f32 = 25.0 / 1000.0;
     const THREAD: f32 = 108.0 / 1000.0;
@@ -275,7 +307,8 @@ async fn main(spawner: Spawner) {
 
     let mut motor_speed;
 
-    spawner.spawn(uart_task(uart_jetson)).unwrap();
+    spawner.spawn(uart_jetson_rx_task()).unwrap();
+    spawner.spawn(uart_jetson_tx_task()).unwrap();
 
     info!("nv1-hub initialized");
 
@@ -285,7 +318,7 @@ async fn main(spawner: Spawner) {
         proc.process_slice(&buf).unwrap();
         parser
             .worker(|frame| {
-                yaw = (frame.as_pretty_frame().yaw as f32).to_radians();
+                yaw = -((frame.as_pretty_frame().yaw as f32).to_radians());
                 // info!("Yaw: {}", yaw);
             })
             .unwrap();
@@ -358,31 +391,25 @@ async fn main(spawner: Spawner) {
         // info!("Line X: {}, Line Y: {}", line_x, line_y);
         // info!("IR X: {}, IR Y: {}", ir_x, ir_y);
 
-        let msgpack = G_MSG_PACK_RX.lock().await.get_mut().clone();
-        angle_pid.setpoint(msgpack.vel.angular_z);
+        let msg = G_MSG_RX.lock().await.clone();
 
-        let angle_pid_output = angle_pid.next_control_output(yaw);
+        // angle_pid.setpoint(msg.vel.angular_z);
+        // let angle_pid_output = angle_pid.next_control_output(yaw);
 
-        let motor1 = wheel_calc1.calculate(
-            msgpack.vel.linear_x,
-            msgpack.vel.linear_y,
-            angle_pid_output.output,
-        );
-        let motor2 = wheel_calc2.calculate(
-            msgpack.vel.linear_x,
-            msgpack.vel.linear_y,
-            angle_pid_output.output,
-        );
-        let motor3 = wheel_calc3.calculate(
-            msgpack.vel.linear_x,
-            msgpack.vel.linear_y,
-            angle_pid_output.output,
-        );
-        let motor4 = wheel_calc4.calculate(
-            msgpack.vel.linear_x,
-            msgpack.vel.linear_y,
-            angle_pid_output.output,
-        );
+        speed_pid.setpoint(msg.vel.angle);
+
+        let now_ms = Instant::now().as_millis();
+        let dt_ms = now_ms - prev_system_time_ms;
+        prev_system_time_ms = now_ms;
+
+        let yaw_speed = (yaw - prev_yaw) / (dt_ms as f32 / 1000.0);
+        prev_yaw = yaw;
+        let speed_pid_output = speed_pid.next_control_output(yaw_speed);
+
+        let motor1 = wheel_calc1.calculate(msg.vel.x, msg.vel.y, -speed_pid_output.output);
+        let motor2 = wheel_calc2.calculate(msg.vel.x, msg.vel.y, -speed_pid_output.output);
+        let motor3 = wheel_calc3.calculate(msg.vel.x, msg.vel.y, -speed_pid_output.output);
+        let motor4 = wheel_calc4.calculate(msg.vel.x, msg.vel.y, -speed_pid_output.output);
 
         motor_speed = MotorSpeed {
             motor1,
@@ -411,65 +438,122 @@ async fn main(spawner: Spawner) {
                 info!("UART write error: {:?}", e);
             }
         };
+
+        let msg_tx = HubMsgPackTx {
+            pause: gpio_ui_toggle.is_high(),
+            shutdown: gpio_ui_enter.is_high(),
+            reboot: false,
+            vel: nv1_msg::hub::Velocity {
+                x: msg.vel.x,
+                y: msg.vel.y,
+                angle: yaw,
+            },
+            ir: nv1_msg::hub::Ir {
+                x: ir_x,
+                y: ir_y,
+                strength: 0.0,
+            },
+            line: nv1_msg::hub::Line {
+                x: line_x,
+                y: line_y,
+                strength: 0.0,
+            },
+            have_ball: false,
+        };
+
+        G_MSG_TX.lock().await.replace(msg_tx);
     }
 }
 
 #[embassy_executor::task]
-async fn uart_task(
-    mut uart_jetson: Uart<
-        'static,
-        peripherals::USART3,
-        peripherals::DMA1_CH3,
-        peripherals::DMA1_CH1,
-    >,
-) {
-    const RX_DATA_SIZE: usize = 57;
-    let msgpack_default = corepack::to_bytes(nv1_msg::HubMsgPackRx::default()).unwrap();
-    let msgpack_data_len: usize = msgpack_default.len();
+async fn uart_jetson_rx_task() {
+    const RX_DATA_SIZE: usize = 15;
+
+    let mut timeout_count = 0;
 
     loop {
-        let mut buf = [0u8; RX_DATA_SIZE];
-        match uart_jetson.read(&mut buf).await {
-            Ok(_) => {
-                let mut cobs_decoded_buf = [0; 64];
-                match corncobs::decode_buf(&buf, &mut cobs_decoded_buf) {
-                    Ok(size) => {
-                        if size != msgpack_data_len {
-                            info!("Invalid data size: {}", size);
+        let mut msg_with_cobs = [0u8; RX_DATA_SIZE];
+        let timeout_res = with_timeout(
+            Duration::from_millis(5),
+            G_UART
+                .lock()
+                .await
+                .as_mut()
+                .unwrap()
+                .read(&mut msg_with_cobs),
+        )
+        .await;
+        match timeout_res {
+            Ok(rx) => match rx {
+                Ok(_) => {
+                    info!("[UART Jetson] received data: {:?}", msg_with_cobs);
+                    match postcard::from_bytes_cobs::<nv1_msg::hub::HubMsgPackRx>(
+                        &mut msg_with_cobs,
+                    ) {
+                        Ok(msg) => {
+                            info!("Linear X: {}", msg.vel.x);
+                            info!("Linear Y: {}", msg.vel.y);
+                            info!("Angular Z: {}", msg.vel.angle);
+
+                            G_MSG_RX.lock().await.vel = msg.vel;
+                        }
+                        Err(_) => {
+                            info!("[UART Jetson] postcard decode error");
                             continue;
                         }
-
-                        let msgpack = corepack::from_bytes::<nv1_msg::HubMsgPackRx>(
-                            &cobs_decoded_buf[0..msgpack_data_len],
-                        )
-                        .unwrap();
-
-                        info!("Linear X: {}", msgpack.vel.linear_x);
-                        info!("Linear Y: {}", msgpack.vel.linear_y);
-                        info!("Angular Z: {}", msgpack.vel.angular_z);
-
-                        G_MSG_PACK_RX.lock().await.get_mut().vel = msgpack.vel
-                    }
-                    Err(err) => {
-                        info!("Failed to decode data");
-                        info!("Data: {:?}", buf);
-                        match err {
-                            corncobs::CobsError::Truncated => info!("Truncated"),
-                            corncobs::CobsError::Corrupt => info!("Corrupt"),
-                        }
-                    }
+                    };
                 }
-            }
-            Err(err) => {
-                if err == usart::Error::Overrun {
-                    info!("Overrun");
-                    continue;
-                } else {
-                    info!("Failed to read data");
-                    info!("Error: {:?}", err);
+                Err(err) => {
+                    info!("[UART Jetson] read error: {:?}", err);
                     continue;
                 }
+            },
+            Err(_) => {
+                timeout_count += 1;
+
+                if timeout_count > 10 {
+                    info!("[UART Jetson] timeout");
+                    G_MSG_RX.lock().await.vel = nv1_msg::hub::Velocity {
+                        x: 0.0,
+                        y: 0.0,
+                        angle: 0.0,
+                    };
+                    timeout_count = 0;
+                }
+                Timer::after_millis(10).await;
+                continue;
             }
-        };
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn uart_jetson_tx_task() {
+    loop {
+        let msg = G_MSG_TX.lock().await.take();
+        match postcard::to_vec_cobs::<nv1_msg::hub::HubMsgPackTx, 64>(&msg) {
+            Ok(msg_with_cobs) => {
+                match G_UART
+                    .lock()
+                    .await
+                    .as_mut()
+                    .unwrap()
+                    .write(&msg_with_cobs)
+                    .await
+                {
+                    Ok(_) => {
+                        // info!("[UART Jetson] sent data, len: {}", msg_with_cobs.len());
+                    }
+                    Err(e) => {
+                        info!("[UART Jetson] write error: {:?}", e);
+                    }
+                };
+            }
+            Err(_) => {
+                info!("[UART Jetson] postcard encode error");
+                continue;
+            }
+        }
+        Timer::after_millis(10).await;
     }
 }
